48b0ed909357661a0b9b840a786fc6afbf6acef0
Gopi Suresh <suresh.gopikrishnan@gmail.com>
2016-09-18 19:30:27 -0700

    Content Added

--- bar.js
+++ bar.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+var bars = document.getElementsByClassName('skill_amount');
+for(var i = 0; i  < bars.length; i++){
+    var l = bars[i].innerHTML.substring(0, bars[i].innerHTML.indexOf("<"));
+    var rest = bars[i].innerHTML.substring(bars[i].innerHTML.indexOf("<"));
+    var level = parseInt(l, 10);
+    bars[i].innerHTML = "";
+    for(var j = 0; j < level; j+=5){
+        bars[i].innerHTML += "#";
+    }
+    bars[i].innerHTML += rest;
+}
+
+function blinkOut(){
+    $(".end_hash").css("background-color", "black");
+    $(".end_hash").css("color", "white");
+}
+function blinkIn(){
+    $(".end_hash").css("background-color", "white");
+    $(".end_hash").css("color", "black");
+    setTimeout(blinkOut, 700);
+}
+setInterval(blinkIn, 1400);
